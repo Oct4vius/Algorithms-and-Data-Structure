b33033154e1de6d70dd43b149119644295108116
Octavio Luperón <armando2445@outlook.com>
2023-12-12 14:43:59 -0400

    cool

--- quick_sort.py
+++ quick_sort.py
@@ -10,15 +10,15 @@ def quick_sort(values):
     if len(values) <= 1:
         return values
     
-    lees_than_pivot = []
+    less_than_pivot = []
     greater_than_pivot = []
     pivot = values[0]
     for value in values[1:]:
         if value <= pivot:
-            lees_than_pivot.append(value)
+            less_than_pivot.append(value)
         else:
             greater_than_pivot.append(value)
-    return quick_sort(lees_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
+    return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
 
 
 l = []
